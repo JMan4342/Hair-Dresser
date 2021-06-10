123e9a18630671459623b7da03e2a7945dbdfecd
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-06-10 12:06:05 -0700

    "test changes"

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,4 @@
-//PAOLA'S FUNCTION FOR GOOGLE MAP
+//PAOLA'S FUNCTION FOR GOOGLE MAPS
 function initMap() {
   map = new google.maps.Map (document.getElementById("map"), {
       center: {lat: 32.24413848450306, lng: -110.9081052153426}, 
@@ -14,3 +14,21 @@ function initMap() {
       window.open("https://goo.gl/maps/ei89BzkWAr8Ht4bs7");
   });
 };
+
+
+//PAOLA'S FUNCTION FOR PEXELS API
+// var apiKey = 
+// var pexelUrl = "https://api.pexels.com/v1/photos/", {
+//     headers: {
+//         Authorization: 
+//     }
+// }
+    
+// fetch(pexelUrl)
+// .then(function (response) {
+//     console.log(response)
+//     return response.json();
+// })
+// .then(function (data) {
+//     console.log(data);
+// })
